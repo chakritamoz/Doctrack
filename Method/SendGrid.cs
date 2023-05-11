2284163b6a65b616d4d0b8fc30a9a286634e0fbb
chakritamoz <chakrit.artamoz@gmail.com>
2023-05-11 17:02:09 +0700

    Update for deploy

--- Method/SendGrid.cs
+++ Method/SendGrid.cs
@@ -11,7 +11,7 @@ namespace Doctrack.SendGrid
       var client = new SendGridClient(apiKey);
       var from = new EmailAddress("chakrit.artamoz@gmail.com");
       var to = new EmailAddress(receipient);
-      var callbackUrl = $"http://localhost:5192/accounts/verifyEmail?token={token}";
+      var callbackUrl = $"https://sangha-doctrack.azurewebsites.net/accounts/verifyEmail?token={token}";
       var subject = "Verify your email address";
       var plainTextContent = $"Please click on the following link to verify your email address: {callbackUrl}";
       var htmlContent = $"Please click on the following link to verify your email address: <a href='{callbackUrl}'>{callbackUrl}</a>";
